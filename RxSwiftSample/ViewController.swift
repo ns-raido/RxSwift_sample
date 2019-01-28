cf119b82c20fa3aabf2cd610937e297b5814ed7a
raido <raido@notespace.jp>
2019-01-28 17:01:32 +0900

    first commit

--- RxSwiftSample/ViewController.swift
+++ RxSwiftSample/ViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+
+// watcher??
+let disposeBag = DisposeBag()
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textFiled: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // orEmptyでStringのみ許容
+        textFiled.rx.text.orEmpty
+            // map内に受け取った値の処理を書く？？
+            // descriptionでStringに変換？？
+            .map {$0.description}
+            // labelとバインド
+            .bind(to: label.rx.text)
+            // 監視終了？？
+            .disposed(by: disposeBag)
     }
 
 
